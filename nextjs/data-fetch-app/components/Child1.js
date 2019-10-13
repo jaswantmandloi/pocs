2d1bda92519d9c86963d2da4d540ac4a3394acd6
Jaswant Mandloi <jaswantmandloi@gmail.com>
2019-10-13 17:24:19 +0530

    POC for useMemo dependencies tracking

--- nextjs/data-fetch-app/components/Child1.js
+++ nextjs/data-fetch-app/components/Child1.js
@@ -1,16 +1,24 @@
-import React, {useState} from 'react'
+import React, {useState, useMemo} from 'react'
 
 function Child1 (props) {
 
   const [state, setState] = useState(0)
+  const {useMemoDeps} = props
 
   console.log('Chidl1 reder happens', props)
+
+  const test123 = useMemo(() => {
+    console.log('running useMemo child1')
+    return `child1 from useMemo in child1`
+  }, [ useMemoDeps])
   
   return (
     <div>
      {props.text}
 
       {state}
+
+      {`useMemoDemo child1: ${test123}`}
 
      <div>
         <button onClick={() => { setState(new Date().getTime())  }} >
--- nextjs/data-fetch-app/components/Test.js
+++ nextjs/data-fetch-app/components/Test.js
@@ -1,45 +1,97 @@
-import React, {useContext, useState} from 'react'
-import {HeaderContext} from '../apis/contexts';
+import React, { useContext, useState, useMemo } from 'react'
+import { HeaderContext } from '../apis/contexts';
 import Child1 from './Child1';
 import Child2 from './Child2';
 
+let useMemoDeps = { test: new Date().getTime() }
 
-function Index (props) {
+function Index(props) {
 
-  const {onSubmit, message} = useContext(HeaderContext);
+  //x === y && (x !== 0 || 1 / x === 1 / y) || x !== x && y !== y
+
+  console.log('sddffsd')
+
+  const { onSubmit, message } = useContext(HeaderContext);
   const [child1, setChild1] = useState('child1')
   const [child2, setChild2] = useState('child2')
+  const [child1UseMemo, setChild1UseMemo] = useState({
+    test1: {
+      test2: {
+        test3: "setChild1UseMemo"
+      }
+    }
+  })
 
-  
+  const test123 = useMemo(() => {
+    console.log('running useMemo', child2)
+    return `${child2} from useMemo`
+  }, [child2, useMemoDeps])
 
-  
-  
+
+
   return (
     <div>
-     {message && <p>{message}</p>}
-      
-      <form onSubmit={onSubmit}>
-      <input type="text" name="" />
 
-      <button>
-        Send
+      {`useMemoDemo: ${test123}`}
+
+      {message && <p>{message}</p>}
+
+      <form onSubmit={onSubmit}>
+        <input type="text" name="" />
+
+        <button>
+          Send
       </button>
 
       </form>
 
-
-      <Child1  text={child1} />
-      <Child2  text={child2} />
-
       <div>
-        <button onClick={() => { setChild1(new Date().getTime())  }} >
-        Update child 1
+        <button onClick={() => {
+
+          // useMemoDeps.test = new Date().getTime()
+
+          setChild1UseMemo({
+            test1: {
+              test2: {
+                test3: new Date().getTime()
+              }
+            }
+          })
+
+        }} >
+          Update useMemo Deps for child1
+      </button>
+      </div>
+
+
+      <hr />
+
+
+      <Child1 text={child1} useMemoDeps={child1UseMemo} />
+      <hr />
+      <Child2 text={child2} />
+      <hr />
+      <div>
+        <button onClick={() => {
+
+          // useMemoDeps.test = new Date().getTime()
+
+          useMemoDeps = { test: new Date().getTime() }
+
+        }} >
+          Update useMemo Deps
         </button>
       </div>
 
       <div>
-        <button onClick={() => { setChild2(new Date().getTime())  }} >
-        Update child 2
+        <button onClick={() => { setChild1(new Date().getTime()) }} >
+          Update child 1
+        </button>
+      </div>
+
+      <div>
+        <button onClick={() => { setChild2(new Date().getTime()) }} >
+          Update child 2
         </button>
       </div>
 
